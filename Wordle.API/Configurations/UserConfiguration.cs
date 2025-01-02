1cec802779fb8b14593ede9d47511bc9954175ab
Ethrynto <ethrynto@outlook.com>
2025-01-02 16:02:58 +0300

    Create a configuration for the User

---
+++ Wordle.API/Configurations/UserConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Wordle.API.Models;
+
+namespace Wordle.API.Configurations;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.ToTable("Users");
+        
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Id)
+            .ValueGeneratedOnAdd();
+        
+        builder.Property(user => user.Username)
+            .IsRequired();
+        
+        builder.Property(user => user.Password)
+            .IsRequired();
+    }
+}
