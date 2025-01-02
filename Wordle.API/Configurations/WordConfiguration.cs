83dd9f63440a44b6563343388a004cdcbd3aa0d9
Ethrynto <ethrynto@outlook.com>
2025-01-02 16:03:05 +0300

    Create a configuration for the Word

---
+++ Wordle.API/Configurations/WordConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Wordle.API.Models;
+
+namespace Wordle.API.Configurations;
+
+public class WordConfiguration : IEntityTypeConfiguration<Word>
+{
+    public void Configure(EntityTypeBuilder<Word> builder)
+    {
+        builder.ToTable("Words");
+        
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Id)
+            .ValueGeneratedOnAdd();
+        
+        builder.Property(word => word.Title)
+            .IsRequired()
+            .HasMaxLength(255);
+
+        builder.Property(word => word.Description)
+            .IsRequired();
+
+        builder.Property(word => word.Length)
+            .IsRequired();
+    }
+}
