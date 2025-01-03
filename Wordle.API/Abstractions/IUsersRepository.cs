3573b69e4a6e4f58edf77a03f1953ebcbd3a273c
Ethrynto <ethrynto@outlook.com>
2025-01-03 20:36:52 +0300

    Create an interface for the User

---
+++ Wordle.API/Abstractions/IUsersRepository.cs
@@ -0,0 +1,11 @@
+﻿using Wordle.API.Models;
+
+namespace Wordle.API.Abstractions;
+
+public interface IUsersRepository
+{
+    Task<IEnumerable<User>> GetUsersAsync();
+    Task<User> GetUserByUserIdAsync(Guid id);
+    Task<User> GetUserByUsernameAsync(string username);
+    
+}
